eabca55df954f03d4cdea344898e4f28defb4d9e
Connie Wong <conniehhw@gmail.com>
2023-01-28 13:58:46 -0500

    Add array of questions, generate html from inputs

--- team-profile-generator/index.js
+++ team-profile-generator/index.js
@@ -1,98 +1,118 @@
-const inquirer = require("inquirer");
-const fs = require("fs");
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateHTML = require('./generateHTML');
 
 // TODO: Create an array of questions for user input
-const questions = [];
-
-// const teamProfiles = [
-//     {
-
-//     }
-// ]
-
-inquirer
-    .createPromptModule([
+const allEmployees = [
         {
-            type: "input",
-            name: "name",
-            message: "Enter the manager's name:", 
+            type: 'input',
+            name: 'name',
+            message: "Enter the manager's name:"
         },
-        {   type: "input",
-            id: "ID",
+        {   type: 'input',
+            name: 'ID',
             message: "Enter the employee ID:",
         },
         {
             type: "input",
-            email: "email address",
+            name: "email address",
             message: "Enter the email address:"
-        },  
+        },   
         {
             type: "input",
-            number: "offce number",
+            name: "offce number",
             message: "Enter the office number:"
         },
 
         {
             type: "list",
-            message: "Select a role to enter next:",
             name: "role",
-            choices: ["Engineer", "Intern"],
-        },
-        {
-            type: "input",
-            name: "name",
-            message: "Enter the employee's name:", 
-        },
-        {   type: "input",
-            id: "ID",
-            message: "Enter the employee's ID:",
-        },
-        {
-            type: "input",
-            email: "email address",
-            message: "Enter the email address:"
-        },
-        {   
-            type: "input",
-            id: "ID",
-            message: "Enter the GitHub username:"
-        },
-        {
-            type: "input",
-            name: "name",
-            message: "Enter the employee's name:", 
-        },
-        {   type: "input",
-            id: "ID",
-            message: "Enter the employee's ID:",
-        },
-        {
-            type: "input",
-            email: "email address",
-            message: "Enter the email address:"
-        },
-        {   
-            type: "input",
-            id: "ID",
-            message: "Enter the school name:"
-        },
-
-        {
-            type: "list",
             message: "Select a role to enter next:",
-            name: "role",
-            choices: ["Engineer", "Intern", "Team Profiles Completed."],
+            choices: ["Engineer", "Intern"]
         },
+        // {
+        //     type: "input",
+        //     name: "name",
+        //     message: "Enter the employee's name:", 
+        // },
+        // {   type: "input",
+        //     name: "ID",
+        //     message: "Enter the employee's ID:"
+        // },
+        // {
+        //     type: "input",
+        //     name: "email address",
+        //     message: "Enter the email address:"
+        // },
+        // {   
+        //     type: "input",
+        //     name: "ID",
+        //     message: "Enter the GitHub username:"
+        // },
+        // {
+        //     type: "input",
+        //     name: "name",
+        //     message: "Enter the employee's name:", 
+        // },
+        // {   type: "input",
+        //     id: "ID",
+        //     message: "Enter the employee's ID:",
+        // },
+        // {
+        //     type: "input",
+        //     name: "email address",
+        //     message: "Enter the email address:"
+        // },
+        // {   
+        //     type: "input",
+        //     name: "ID",
+        //     message: "Enter the school name:"
+        // },
 
-    ])
+        // {
+        //     type: "list",
+        //     message: "Select a role to enter next:",
+        //     name: "role",
+        //     choices: ["Engineer", "Intern", "Team Profiles Completed."]
+        // },
+    ]
 
-    .then((data) => {
-        const filename = `${data.name}.json`;
+inquirer.prompt(allEmployees)
+    .then((answers) => {
+    fs.writeFile('index.html', generateHTML(answers), function (err) {
+        if (err) throw err;
+        console.log('Saved');
+    })
+});
+
+
+// writeFile();
+
+// function writeToFile() {
+//     return inquirer.prompt(allEmployees)
+//         .then((employees => {
+//         fs.writeFileSync('index.html', generateHTML(allEmployees), function(err) {
+//             if(err) {
+//                 console.log('Could not generate team profile')
+//             } else {
+//                 console.log('Success: new team profile created')
+//                 }
+//             })
+//          })
+//         .catch((error) => {
+//             console.log(error)
+//         }))
+//  }
+
+//  writeToFile();
+  
+    // .then((data) => {
+    //     const filename = `${data.name}.json`;
     
-        fs.writeFile(filename, JSON.stringify(data, null, "\t"), (err) =>
-          err ? console.log(err) : console.log("Success!")
-        );
-      });
+    //     fs.writeFile(filename, JSON.stringify(data, null, "\t"), (err) =>
+    //       err ? console.log(err) : console.log("Success!")
+    //     );
+    //   });
 
         // TODO: Create a function to write README file
-    function writeToFile(fileName, data) {}
+    // function writeToFile(fileName, data) {}
