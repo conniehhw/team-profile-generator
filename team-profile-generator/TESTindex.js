d0f70418b9850ba93a27784797113832f152493f
Connie Wong <conniehhw@gmail.com>
2023-01-31 13:07:22 -0500

    Fix styling

--- team-profile-generator/TESTindex.js
+++ team-profile-generator/TESTindex.js
@@ -83,39 +83,36 @@ const questions2 = [
     },
 ];
 
+const questions3 = [
+    {
+        type: 'list',
+        name: 'role',
+        message: "Select the role you wish to add next:(use arrow keys)",
+        choices: ['Engineer', 'Intern']
+    },
+    {   
+        type: 'input',
+        name: 'name',
+        message: "Enter the employee's name:",
+    },
+    {   
+        type: 'input',
+        name: 'ID',
+        message: "Enter the employee's ID:", 
+    },
+    {
+        type: 'input',
+        name: "email",
+        message: "Enter the email address:",
+    },
+    {
+        type: 'input',
+        name: "school",
+        message: "Enter the school name:",
+    },
+];
 
-// function writeToFile() {
-//     return inquirer.prompt(questions)
-//         .then((answers)=> {
-//             const mark = MarkDown.generateMarkdown(answers)
-//             fs.writeFile('index.html', mark, function(err) {
-//                 if(err) { 
-//                     console.log('Could not generate file')
-//                 } else {
-//                     console.log('Success: new index.html generated inside the current folder')
-//                 }
-//             })
-//         })
-//         .catch((error)=> {
-//             console.log(error)
-//         })
-// }
 
-// writeToFile();
-        
-// const appendFile = async (filePath) => {
-//     try {
-//         const data = await fs.readFile('test.txt', 'utf8');
-//         const contents = await fs.appendFile(filePath, data);
-//         await fs.writeFile('newFile.txt', data); //create newfile with what is read from test.txt
-//         console.log(contents);
-//     } catch (err) {
-//         console.error(err.message);
-//     }
-// }
-
-// appendFile('log.txt'); //add what is read from test.txt
-/////////////////
 
 const appendFile = async (filePath) => {
     try {
@@ -124,12 +121,19 @@ const appendFile = async (filePath) => {
             await fs.writeFile('index.html', mark);
         const answers1 = await inquirer.prompt(questions1);
         const mark1 = MarkDown.generateMarkdown1(answers1);
-            const contents = await fs.appendFile(filePath, mark1);
+        const contents = await fs.appendFile(filePath, mark1);
+
         const answers2 = await inquirer.prompt(questions2);
         const mark2 = MarkDown.generateMarkdown2(answers2);
-            const contents1 = await fs.appendFile(filePath, mark2);
+        const contents1 = await fs.appendFile(filePath, mark2);
+
+        const answers3 = await inquirer.prompt(questions3);
+        const mark3 = MarkDown.generateMarkdown2(answers3);
+        const contents2 = await fs.appendFile(filePath, mark3);
+        
         console.log(contents);
         console.log(contents1);
+        console.log(contents2);
     } catch (err) {
         console.error(err.message);
     }
--- team-profile-generator/generateMarkdown.js
+++ team-profile-generator/generateMarkdown.js
@@ -1,52 +1,85 @@
 class MarkDown {
 
-        static generateMarkdown(answers) {
-    return `
+    static generateMarkdown(answers) {
+        return `
     <!DOCTYPE html>
     <html lang="en">
     <head>
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="style.css">
+        <link rel="stylesheet" href="./dist/style.css">
         <title>Document</title>
     </head>
     <body>
+
+    
+<h1>My Team</h1>
+    <div class="main">
         
-    <section class="user-card">
-        <h1>${answers.name}</h1>
-        <h2>Manager</h2>
-        <p>ID: ${answers.ID}</p>
-        <p>Email: ${answers.email}</p>
-        <p>Office Number: ${answers.office}</p>
-    </section>
+    <div class="card">
+        <div class="header">
+            <h2>${answers.name}</h2>
+         </div>
+         <div class="container">
+            <h3>Manager</h3>
+            <p>ID: ${answers.ID}</p>
+            <p>Email: ${answers.email}</p>
+            <p>Office Number: ${answers.office}</p>
+        </div>
+    </div>
             `
         }
 
-        static generateMarkdown1(answers1) {
-                return ` 
-                <section class="user-card">
-                <h1>Engineer</h1>
-                <h2>${answers1.name}</h2>
-                <p>ID: ${answers1.ID}</p>
-                <p>Email: ${answers1.email}</p>
-                <p>Git Hub username: ${answers1.username}</p>
-            </section>
+    static generateMarkdown1(answers1) {
+        return ` 
+    <div class="card">
+        <div class="header">
+        <h2>${answers1.name}</h2>
+        </div>
+        <div class="container">
+            <h3>Engineer</h3>    
+            <p>ID: ${answers1.ID}</p>
+            <p>Email: ${answers1.email}</p>
+            <p>Git Hub username: ${answers1.username}</p>
+    </div>
+    </div>
                 `
         }
 
-        static generateMarkdown2(answers2) {
-                    return `
-               <section class="user-card">
-                <h1>Intern</h1>
-                <h2>${answers2.name}</h2>
-                <p>ID: ${answers2.ID}</p>
-                <p>Email: ${answers2.email}</p>
-                <p>School: ${answers2.school}</p>
-            </section>
+    static generateMarkdown2(answers2) {
+            return `
+    <div class="card">
+        <div class="header">
+        <h2>${answers2.name}</h2>
+        </div>
+        <div class="container">
+            <h3>Intern</h3>
+            <p>ID: ${answers2.ID}</p>
+            <p>Email: ${answers2.email}</p>
+            <p>School: ${answers2.school}</p>
+        </div> 
+    </div>`
+    }
+
+    static generateMarkdown3(answers3) {
+        return `
+    <div class="card">
+        <div class="header">
+        <h2>${answers3.name}</h2>
+        </div>
+        <div class="container">
+            <h3>Intern</h3>
+            <p>ID: ${answers3.ID}</p>
+            <p>Email: ${answers3.email}</p>
+            <p>School: ${answers3.school}</p>
+    </div>
+</div>
+
+</div>
     
-    </body>
-    </html>
+</body>
+</html>
     `
     }
 
