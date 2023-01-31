28b457a753914176ee94ac094b96b0b9a13825a6
Connie Wong <conniehhw@gmail.com>
2023-01-31 16:06:56 -0500

    Add rest of classes

--- team-profile-generator/lib/class.js
+++ team-profile-generator/lib/class.js
@@ -1,21 +1,4 @@
-// class Car {
-//     constructor(name, id, email, role) {
-//         this.name = name;
-//         this.id = id;
-//         this.email = email;
-//         this.role = role;
-//     }
-//     printInfo() {
-//         console.log(`Name: ${this.name}`);
-//         console.log(`id: ${this.id}`);
-//     }
-// }
-    
-// const myCar1 = new Car("Ruby", "45", "@email.com", "Manager");
-// const myCar2 = new Car("Rupert", "55", "@email.com", "Engineer");
-
-// myCar1.printInfo();
-
+// Create classes & constructor methods for inheritance
 class Employee {
     constructor(name, ID, email) {
         this.name = name;
@@ -72,7 +55,50 @@ class Manager extends Employee {
 const manager = new Manager ("Rupert ", "345 ", "j@email.com ", "999");
 manager.show();
 
+class Engineer extends Employee {
+    constructor(name, ID, email, username) {
+        super(name, ID, email, username);
+        this.ghusername = username;
+    }
+        getRole() {
+            return "Engineer";
+        }
 
+        getUsername() {
+            return this.ghusername,
+            console.log(this.ghusername)
+        }
+
+        show() {
+            return this.present() + this.getUsername();
+        }
+    }
+
+const engineer = new Engineer ("Ryan ", "999 ", "cat@email.com ", "ryanryan");
+engineer.show();
+
+
+class Intern extends Employee {
+    constructor(name, ID, email, school) {
+        super(name, ID, email, school);
+        this.school = school;
+    }
+        getRole() {
+            return "Intern";
+        }
+
+        getSchoolname() {
+            return this.school,
+            console.log(this.school)
+        }
+
+        show() {
+            return this.present() + this.getSchoolname();
+        }
+    }
+
+const intern = new Intern ("Connie ", "432 ", "dog@email.com ", "UofT");
+intern.show();
 
 // myEmployee.show();
 
